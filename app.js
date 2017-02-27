9d5b6a586605da307c15db00a23c75c30bd884b7
jdfandino10 <jd.fandino10@uniandes.edu.co>
2017-02-27 15:12:04 -0500

    get de usuario de prueba, mongolab ya esta en linea

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/*app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +38,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient,
+	assert = require('assert');
+
+var url = 'mongodb://server:serverPass1@ds163718.mlab.com:63718/smart_planner';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendfile('index.html');
 });
+
+router.get('/users', function (req, res) {
+  console.log("llego al get de users");
+  getUser(function (users) {
+  	console.log(users);
+  	res.setHeader('Content-Type', 'application/json');
+  	res.send(JSON.stringify(users));
+  });
+});
+
+function getUser(callback) {
+	MongoClient.connect(url, function(err, db){
+		assert.equal(null, err);
+
+		var tweetsCol = db.collection("Users");
+		tweetsCol.find({'user_name':'default'}).toArray(function(err, data){
+			assert.equal(null, err);
+			console.log("got data?");
+			callback(data);
+		});
+	});
+}
 
 module.exports = router;
