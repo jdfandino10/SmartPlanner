fef719c188b45e302c84b7795bd47cba026956c8
jdfandino10 <jd.fandino10@uniandes.edu.co>
2017-03-01 15:58:47 -0500

    post hmk hecho

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient,
-	assert = require('assert');
+assert = require('assert');
+var ObjectId = require('mongodb').ObjectId; 
 
 var url = 'mongodb://server:serverPass1@ds163718.mlab.com:63718/smart_planner';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile('index.html');
+	res.sendfile('index.html');
 });
 
 router.get('/users', function (req, res) {
-  console.log("llego al get de users");
-  getUser(function (users) {
-  	console.log(users);
-  	res.setHeader('Content-Type', 'application/json');
-  	res.send(JSON.stringify(users));
-  });
+	getUser(function (users) {
+		res.setHeader('Content-Type', 'application/json');
+		res.send(JSON.stringify(users));
+	});
+});
+
+router.post('/hmk', function (req, res) {
+	var id = req.body.userId;
+	var hmk = req.body.hmk;
+	console.log("id: "+id);
+	console.log("hmk: "+hmk);
+	addHmkToUser(id, hmk, function(obj){
+		res.setHeader('Content-Type', 'application/json');
+		res.send(JSON.stringify(obj));
+	});
 });
 
 function getUser(callback) {
@@ -26,8 +36,20 @@ function getUser(callback) {
 		var tweetsCol = db.collection("Users");
 		tweetsCol.find({'user_name':'default'}).toArray(function(err, data){
 			assert.equal(null, err);
-			console.log("got data?");
 			callback(data);
+		});
+	});
+}
+
+function addHmkToUser(id, hmk, callback) {
+	MongoClient.connect(url, function(err, db){
+		assert.equal(null, err);
+
+		var tweetsCol = db.collection("Users");
+		tweetsCol.update({'_id': ObjectId(id)}, {$push: {'hmk':hmk}}, {}, function(err, object){
+			assert.equal(null, err);
+
+			callback(object);
 		});
 	});
 }
